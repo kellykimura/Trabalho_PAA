9991f0f9686e21ebac93168c552e2c72e1c9e5fe
Kelly Kimura <akellykimura007@gmail.com>
2024-09-20 17:19:06 -0300

    bubbleSort

--- Trabalho_Prático1/projPython.py
+++ Trabalho_Prático1/projPython.py
@@ -3,16 +3,32 @@ def bubbleSort (vetor):
 
     # for para contar as passagens pelo vetor
     # n rebe o tam do vetor, para qnd chegar em 0, e decresce de 1 em 1
-    for n in range(len(vetor) -1, 0, -1):
+    for i in range(len(vetor) -1, 0, -1):
 
         #for para andar pelas posições do vetor
-        for i in range(n):
-            if vetor[i] > vetor[i+1]:
-                swapped = True
-                # trocando as posições dos elementos
-                vetor[i], vetor[i+1] = vetor[i+1], vetor[i]
+        for j in range(i):
+            if vetor[j] > vetor[j+1]:
+                # trocando as posjções dos elementos
+                vetor[j], vetor[j+1] = vetor[j+1], vetor[j]
 
 # como chamar
 # bubbleSort(vetor)
 
+def bubbleSortMelhorado (vetor):
+    
+    for i in range(1, len(vetor), 1):
+        swapped = False
+
+        for j in range(len(vetor) - 1 - i):
+            if vetor[j] > vetor[j+1]:
+                vetor[j], vetor[j+1] = vetor[j+1], vetor[j]
+                swapped = True
+
+            if not swapped:
+                break
+
+vetor = [5, 2, 9, 1, 8, 6]
+bubbleSort(vetor)
+#bubbleSortMelhorado(vetor)
+print(vetor)
 
