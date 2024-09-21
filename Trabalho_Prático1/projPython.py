69864c6867d575efce3da6a2cd2b997765b8f8d7
Kelly Kimura <akellykimura007@gmail.com>
2024-09-21 09:06:11 -0300

    quickSort

--- Trabalho_Prático1/projPython.py
+++ Trabalho_Prático1/projPython.py
@@ -14,6 +14,8 @@ def bubbleSort (vetor):
 # como chamar
 # bubbleSort(vetor)
 
+
+# 2. Bubble Sort Melhorado
 def bubbleSortMelhorado (vetor):
     
     for i in range(1, len(vetor), 1):
@@ -27,8 +29,42 @@ def bubbleSortMelhorado (vetor):
             if not swapped:
                 break
 
-vetor = [5, 2, 9, 1, 8, 6]
-bubbleSort(vetor)
+
+# 3. Quick Sort
+def particaoQuickSort (vetor, esquerda, direita):
+    pivo = vetor[esquerda]
+    i = esquerda + 1
+    j = direita
+
+    while i <= j:
+
+        while i <= direita and vetor[i] <= pivo:
+            i += 1
+
+        while j >= esquerda and vetor[j] > pivo:
+            j -= 1
+
+        if i < j:
+            vetor[i], vetor[j] = vetor[j], vetor[i]
+            i += 1
+            j -= 1
+        
+    
+    vetor[esquerda], vetor[j] = vetor[j], vetor[esquerda]
+    return j
+
+def quickSort (vetor, esquerda, direita):
+    if esquerda < direita:
+        meio = particaoQuickSort(vetor, esquerda, direita)
+        quickSort(vetor, esquerda, meio - 1)
+        quickSort(vetor, meio + 1, direita)
+        
+        
+
+vetor = [5, 2, 9, 13, 1, 23, 45, 3, 8, 6]
+#bubbleSort(vetor)
 #bubbleSortMelhorado(vetor)
+
+quickSort(vetor, 0, len(vetor) - 1)
 print(vetor)
 
